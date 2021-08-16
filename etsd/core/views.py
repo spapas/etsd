3a5a463806e7f4f9524787eb40760c2cb884dadf
Serafeim Papastefanos <spapas@gmail.com>
2021-08-16 16:12:42 +0300

    run black

--- etsd/core/views.py
+++ etsd/core/views.py
@@ -10,32 +10,39 @@ from dal import autocomplete
 from django.db.models import Q
 
 
-class AuthorityEditUsersView(UpdateView, ):
+class AuthorityEditUsersView(
+    UpdateView,
+):
     model = Authority
     form_class = AuthorityUsersModelForm
-    template_name='core/edit_authority_data.html'
+    template_name = "core/edit_authority_data.html"
 
     def get_form_kwargs(self):
         kwargs = super(AuthorityEditUsersView, self).get_form_kwargs()
-        kwargs['request'] = self.request
+        kwargs["request"] = self.request
         return kwargs
 
-    def form_valid(self,form):
-        new_users = form.cleaned_data['users']
+    def form_valid(self, form):
+        new_users = form.cleaned_data["users"]
         auth = form.instance
         initial_users = auth.users.all()
         added_users = set(new_users).difference(set(initial_users))
         removed_users = set(initial_users).difference(set(new_users))
 
-        user_permission = Permission.objects.get(codename="user", content_type__model="globalpermissionholder")
+        user_permission = Permission.objects.get(
+            codename="user", content_type__model="globalpermissionholder"
+        )
         for usr in added_users:
             usr.user_permissions.add(user_permission)
         for usr in removed_users:
             usr.user_permissions.remove(user_permission)
 
         form.save()
-        messages.add_message(self.request, messages.INFO, _("Authority Data succesfully updated!"))
+        messages.add_message(
+            self.request, messages.INFO, _("Authority Data succesfully updated!")
+        )
         return HttpResponseRedirect(self.request.path)
+
 
 class AuthorityAutocomplete(autocomplete.Select2QuerySetView):
     def get_queryset(self):
@@ -46,6 +53,10 @@ class AuthorityAutocomplete(autocomplete.Select2QuerySetView):
         qs = Authority.objects.all().exclude(id=self.request.user.get_authority().id)
 
         if self.q:
-            qs = qs.filter(Q(name__icontains=self.q) | Q(code__icontains=self.q) | Q(email__icontains=self.q))
+            qs = qs.filter(
+                Q(name__icontains=self.q)
+                | Q(code__icontains=self.q)
+                | Q(email__icontains=self.q)
+            )
 
         return qs
