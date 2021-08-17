547efed5f1eab58afee63ccdc10b220f3ef94baa
nlagkas <nlagkas@hcg.gr>
2021-08-17 08:54:44 +0300

    get admin mails

--- etsd/users/utils.py
+++ etsd/users/utils.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import Permission
 from . import models
 
 def get_authority_users(authority):
@@ -7,4 +8,4 @@ def get_authority_users_emails(authority):
     return [usr.email for usr in get_authority_users(authority)]
 
 def get_admin_emails():
-    return [usr.email for usr in models.User.objects.filter(is_staff=1)]
+    return [usr.email for usr in models.User.objects.filter(user_permissions__codename='admin')]
