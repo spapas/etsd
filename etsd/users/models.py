c8be6e161cf46fdca2d3296e6ce511a224f6cf18
Serafeim Papastefanos <spapas@gmail.com>
2021-09-15 09:16:32 +0300

    Add a user log model

--- etsd/users/models.py
+++ etsd/users/models.py
@@ -1,6 +1,9 @@
+from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import ugettext as _
 from memoize import memoize
+from authorities.models import Authority
+from etsd.core.models import UserDateAbstractModel
 
 # Use custom user model by default as per
 # https://docs.djangoproject.com/en/2.0/topics/auth/customizing/#using-a-custom-user-model-when-starting-a-project
@@ -11,3 +14,9 @@ class User(AbstractUser):
     @memoize(timeout=60)
     def get_authority(self):
         return self.authorities.all().first()
+
+
+class UserManagementLog(UserDateAbstractModel):
+    authority = models.ForeignKey(Authority, editable=False, on_delete=models.PROTECT)
+    removed_users = models.TextField(editable=False, blank=True, default="")
+    added_users = models.TextField(editable=False, blank=True, default="")
