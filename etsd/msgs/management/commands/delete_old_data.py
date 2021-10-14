20e7a5e23e5a579e631b89a47524e95e4253b4f2
nlagkas <nlagkas@hcg.gr>
2021-10-14 10:53:42 +0300

    mcommand old_data add flag

--- etsd/msgs/management/commands/delete_old_data.py
+++ etsd/msgs/management/commands/delete_old_data.py
@@ -11,6 +11,15 @@ from django.utils.translation import ugettext_lazy as _
 class Command(BaseCommand):
     help = "Delete data  older than a year (from the date the message was sent). Notify authorities for data to be deleted after 10 days."
 
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--no_dry",
+            action="store_true",
+            default=False,
+            help="Include this option to actually delete",
+        )
+    
+
     def handle(self, *args, **kwargs):
         one_year_before = timezone.now() - timezone.timedelta(days=365)
         messages_to_delete = models.Message.objects.filter(
@@ -22,11 +31,9 @@ class Command(BaseCommand):
 
         if del_cdata:
             self.stdout.write(self.style.WARNING("Data from the following messages will be deleted:"))
-            self.stdout.write("%s" %messages_to_delete)
-            for cdata in del_cdata:
-                cdata.delete()
-            self.stdout.write(self.style.WARNING("Deleted data:"))
-            self.stdout.write("%s" %del_cdata)
+            self.stdout.write("{}".format(messages_to_delete))
+            self.stdout.write(self.style.WARNING("Data to be deleted:"))
+            self.stdout.write("{}".format(del_cdata))
 
 
         messages_to_notify = models.Message.objects.filter(
@@ -43,19 +50,30 @@ class Command(BaseCommand):
         for k, v in participants_to_notify:
             participants_to_notify_dict[k].append(v)
 
-        for k in participants_to_notify_dict:
-            email_body = send_mail_body(
-                "msgs/emails/notify_data_deletion.txt",
-                dict(messages=participants_to_notify_dict[k]),
-            )
-            send_mail(
-                subject=_("Old message data to be deleted"),
-                message=email_body,
-                from_email="noreply@hcg.gr",
-                recipient_list=[k.email],
-                fail_silently=False,
-            )
+        
 
         if messages_to_notify:
-            self.stdout.write(self.style.SUCCESS("Messages to be deleted within 10 days (notified participants):"))
-            self.stdout.write("%s" %messages_to_notify)
+            self.stdout.write(self.style.SUCCESS("Messages to be deleted within 10 days:"))
+            self.stdout.write("{}".format(messages_to_notify))
+            self.stdout.write(self.style.SUCCESS("Participants to be notified:"))
+            self.stdout.write("{}".format(messages_to_notify))
+            for k in participants_to_notify_dict:
+                self.stdout.write("{0} : {1}".format(k,k.email))
+
+        if kwargs['no_dry']:
+            del_cdata.delete()
+            for k in participants_to_notify_dict:
+                email_body = send_mail_body(
+                    "msgs/emails/notify_data_deletion.txt",
+                    dict(messages=participants_to_notify_dict[k]),
+                )
+                send_mail(
+                    subject=_("Old message data to be deleted"),
+                    message=email_body,
+                    from_email="noreply@hcg.gr",
+                    recipient_list=[k.email],
+                    fail_silently=False,
+                )
+            self.stdout.write(self.style.SUCCESS("Operations completed."))
+        else:
+            self.stdout.write(self.style.WARNING("Run command with --no_dry to complete above operations."))
