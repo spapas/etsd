009ed18d08261a8f02a4ff9838c596e48aa43c6a
nlagkas <nlagkas@hcg.gr>
2022-01-12 10:39:11 +0200

    revertin to prev state

--- etsd/msgs/management/commands/delete_old_data.py
+++ etsd/msgs/management/commands/delete_old_data.py
@@ -19,40 +19,35 @@ class Command(BaseCommand):
             default=False,
             help="Include this option to actually delete",
         )
+    
 
     def handle(self, *args, **kwargs):
-        one_year_before = timezone.now() - timezone.timedelta(days=1095)
+        one_year_before = timezone.now() - timezone.timedelta(days=67)
         one_year_before_p10 = one_year_before + timezone.timedelta(days=10)
         self.stdout.write("Will delete messages older than: {}".format(one_year_before))
-        self.stdout.write(
-            "Will notify for messages older than: {}".format(one_year_before_p10)
-        )
+        self.stdout.write("Will notify for messages older than: {}".format(one_year_before_p10))
+
 
         messages_to_delete = models.Message.objects.filter(
-            Exists(models.CipherData.objects.filter(data__message=OuterRef("pk"))),
-            sent_on__lte=one_year_before,
+            Exists(models.CipherData.objects.filter(data__message = OuterRef('pk'))),
+            sent_on__lte=one_year_before
         )
 
-        del_cdata = models.CipherData.objects.filter(
-            data__message__in=messages_to_delete
-        )
+        del_cdata = models.CipherData.objects.filter(data__message__in=messages_to_delete)
 
         if del_cdata:
-            self.stdout.write(
-                self.style.WARNING("Data from the following messages will be deleted:")
-            )
-            self.stdout.write(
-                "{}".format(", ".join(str(x) for x in messages_to_delete))
-            )
+            self.stdout.write(self.style.WARNING("Data from the following messages will be deleted:"))
+            self.stdout.write("{}".format(", ".join(str(x) for x in messages_to_delete)))
             self.stdout.write(self.style.WARNING("Data to be deleted:"))
             self.stdout.write("{}".format(", ".join(str(x) for x in del_cdata)))
 
+        
         messages_to_notify = models.Message.objects.filter(
-            Exists(models.CipherData.objects.filter(data__message=OuterRef("pk"))),
-            sent_on__lte=one_year_before_p10,
+            Exists(models.CipherData.objects.filter(data__message = OuterRef('pk'))),
+            sent_on__lte=one_year_before_p10
         )
 
-        participants_to_notify = []
+        participants_to_notify= []
         for message in messages_to_notify:
             for participant in models.Participant.objects.filter(message=message):
                 participants_to_notify.append((participant.authority, message))
@@ -62,29 +57,23 @@ class Command(BaseCommand):
             participants_to_notify_dict[k].append(v)
 
         if messages_to_notify:
-            self.stdout.write(
-                self.style.SUCCESS("Messages to be deleted within 10 days:")
-            )
-            self.stdout.write(
-                "{}".format(", ".join(str(x) for x in messages_to_notify))
-            )
+            self.stdout.write(self.style.SUCCESS("Messages to be deleted within 10 days:"))
+            self.stdout.write("{}".format(", ".join(str(x) for x in messages_to_notify)))
             self.stdout.write(self.style.SUCCESS("Participants to be notified:"))
             for k in participants_to_notify_dict:
                 self.stdout.write("{0}: {1}".format(k, k.email))
 
-        if kwargs["no_dry"]:
+        if kwargs['no_dry']:
             del_cdata.delete()
+
 
         for k in participants_to_notify_dict:
             email_body = send_mail_body(
                 "msgs/emails/notify_data_deletion.txt",
-                dict(
-                    messages=participants_to_notify_dict[k],
-                    one_year_before_p10=one_year_before_p10,
-                ),
+                dict(messages=participants_to_notify_dict[k], one_year_before_p10=one_year_before_p10),
             )
             recipients = get_authority_users_emails(k)
-            if kwargs["no_dry"] and len(recipients) > 0:
+            if kwargs['no_dry'] and len(recipients) > 0:
                 send_mail(
                     subject=_("[ETSD] Old message data will be deleted"),
                     message=email_body,
@@ -93,19 +82,11 @@ class Command(BaseCommand):
                     fail_silently=False,
                 )
             else:
-                self.stdout.write(
-                    "Will send emails to: {} for authority {}".format(
-                        ",".join(recipients) or "-", k
-                    )
-                )
+                self.stdout.write("Will send emails to: {} for authority {}".format(",".join(recipients) or '-', k))
                 self.stdout.write(email_body)
                 self.stdout.write("--------------")
 
-        if kwargs["no_dry"]:
+        if kwargs['no_dry']:
             self.stdout.write(self.style.SUCCESS("Operations completed."))
         else:
-            self.stdout.write(
-                self.style.WARNING(
-                    "Run command with --no_dry to complete above operations."
-                )
-            )
+            self.stdout.write(self.style.WARNING("Run command with --no_dry to complete above operations."))
