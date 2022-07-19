7e67118d136f71a2aa4a6a5bfd183f723303f10f
nlagkas <nlagkas@hcg.gr>
2022-07-19 11:28:15 +0300

    filter by sender

--- etsd/msgs/filters.py
+++ etsd/msgs/filters.py
@@ -1,8 +1,24 @@
 import django_filters
 from . import models
+from django.forms import DateInput
+from django.utils.translation import gettext as _
 
+class SenderFilter(django_filters.Filter):
+    def filter(self, qs, value):
+        if value:
+            new_qs = qs
+            for q in qs:
+                if not value in q.message.sender[0].authority.name:
+                    new_qs = new_qs.exclude(id=q.id)
+            return new_qs
+        return qs
 
 class MessageFilter(django_filters.FilterSet):
+    sent_on = django_filters.DateFilter(
+            'sent_on__date', 
+            label = _("Sent on"),
+            widget=DateInput(attrs={'class': 'datepicker'})
+        )  
     class Meta:
         model = models.Message
         fields = {
@@ -11,7 +27,7 @@ class MessageFilter(django_filters.FilterSet):
             "status": ["exact"],
             "protocol": ["exact"],
             "protocol_year": ["exact"],
-            "sent_on": ["exact", "month", "year"],
+            #"sent_on": ["exact", "month", "year"],
         }
 
     def __init__(self, *args, **kwargs):
@@ -19,8 +35,15 @@ class MessageFilter(django_filters.FilterSet):
 
 
 class ParticipantFilter(django_filters.FilterSet):
+    sent_on = django_filters.DateFilter(
+            'message__sent_on__date', 
+            label = _("Sent on"),
+            widget=DateInput(attrs={'class': 'datepicker'})
+        )  
+    sendr = SenderFilter()
     class Meta:
         model = models.Participant
+             
         fields = {
             "status": ["exact"],
             "kind": ["exact"],
@@ -28,7 +51,7 @@ class ParticipantFilter(django_filters.FilterSet):
             "message__status": ["exact"],
             "message__protocol": ["exact"],
             "message__protocol_year": ["exact"],
-            "message__sent_on": ["exact", "month", "year"],
+            #"message__sent_on": ["exact", "month", "year"],
             "message__rel_message__protocol": [
                 "exact",
             ],
@@ -36,3 +59,9 @@ class ParticipantFilter(django_filters.FilterSet):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.filters['message__kind'].label=_("Message kind")
+        self.filters['message__status'].label=_("Message status")
+        self.filters['message__protocol'].label=_("Protocol")
+        self.filters['message__protocol_year'].label=_("Protocol year")
+        self.filters['message__rel_message__protocol'].label=_("Related message")
+        self.filters['sendr'].label=_("Sender")
